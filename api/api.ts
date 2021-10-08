0f9305b553f2f50f7e27feaa9325a5a72bad97c4
MollyBlanket <stasnetreba1322@gmail.com>
2021-10-08 10:03:24 +0300

    bug fixed

--- api/api.ts
+++ api/api.ts
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 const tanks_info = require('../jsons/tanks_info.json');
 const errorsMessages = require('../jsons/errorsMessages.json');
 const config = require('../jsons/config.json');
@@ -19,7 +19,7 @@ export class WotBAPI {
         if (name.split(/\s+/g).length > 1) throw new Error('ERROR_STRING_MUST_BE_WITHOUT_SPACES');
 
         let link: string = `${this.urls.getUserIdByName}${name}`;
-        let response = await fetch(link, { method: 'GET' }).then((res) => res.json());
+        let response = await fetch(link, { method: 'GET' }).then((res: any) => res.json());
         if (!response?.data?.[0]) throw new Error('ERROR_COULD_NOT_FIND_PLAYER');
         let account_id: number = response.data[0].account_id;
         return account_id;
@@ -27,7 +27,7 @@ export class WotBAPI {
     async getUserStatisticById(id: number | string) {
         if (typeof id !== 'number' && !parseInt(id)) throw new Error('ERROR_ARGUMENT_IS_NOT_ID');
         let link: string = `${this.urls.getUserStatistic}${id}`;
-        let response = await fetch(link, { method: 'GET' }).then((res) => res.json());
+        let response = await fetch(link, { method: 'GET' }).then((res: any) => res.json());
 
         if (!response?.data[id]) throw new Error('ERROR_COULD_NOT_FIND_PLAYER');
         let statistics: Record<string, Record<string, number>> = response.data[id].statistics;
@@ -44,7 +44,7 @@ export class WotBAPI {
         if (this.tanks_info[id]) return this.tanks_info[id];
         // Если нету в спсиске (сделано во избежании ошибки 429)
         let link: string = `${this.urls.getTankById}${id}`;
-        let response = await fetch(link, { method: 'GET' }).then((res) => res.json());
+        let response = await fetch(link, { method: 'GET' }).then((res: any) => res.json());
 
         let tank: Record<string, any> | undefined = response.data?.[id];
         return tank;
@@ -53,7 +53,7 @@ export class WotBAPI {
         if (typeof id !== 'number' && !parseInt(id)) throw new Error('ERROR_ARGUMENT_IS_NOT_ID');
 
         let link: string = `${this.urls.getAngarStatistic}${id}`;
-        let response = await fetch(link, { method: 'GET' }).then((res) => res.json());
+        let response = await fetch(link, { method: 'GET' }).then((res: any) => res.json());
 
         if (!response.data[id]) throw new Error('ERROR_COULD_NOT_FIND_TANK');
         let statistic: Array<Record<string, any>> = response.data[id];
